a219f3f068499ba7269d1892eb6b1862ae627843
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-02-04 14:27:31 -0600

    switched if,else to ternary.  my linter keeps giving me errors for some reason, but it all works.

--- javascripts/DOMHandler.js
+++ javascripts/DOMHandler.js
@@ -23,73 +23,41 @@ $("#bread-chooser").change( () => {
 //Cheese Checkboxes
 $("input[value='american']").click( () => {
 	var cheesePrice = SandwichMaker.addCheese($("input[value='american']").val());
-	if( $("input[value='american']").is(":checked")) {
-		SandwichMaker.addTopping(cheesePrice);
-	} else {
-		SandwichMaker.removeTopping(cheesePrice);
-	}
+	$("input[value='american']").is(":checked") ? SandwichMaker.addTopping(cheesePrice) : SandwichMaker.removeTopping(cheesePrice);
 });
 $("input[value='swiss']").click( () => {
 	var cheesePrice = SandwichMaker.addCheese($("input[value='swiss']").val());
-	if( $("input[value='swiss']").is(":checked")) {
-		SandwichMaker.addTopping(cheesePrice);
-	} else {
-		SandwichMaker.removeTopping(cheesePrice);
-	}
+	$("input[value='swiss']").is(":checked") ? SandwichMaker.addTopping(cheesePrice) : SandwichMaker.removeTopping(cheesePrice);
 });
 $("input[value='cheddar']").click( () => {
 	var cheesePrice = SandwichMaker.addCheese($("input[value='cheddar']").val());
-	if( $("input[value='cheddar']").is(":checked")) {
-		SandwichMaker.addTopping(cheesePrice);
-	} else {
-		SandwichMaker.removeTopping(cheesePrice);
-	}
+	$("input[value='cheddar']").is(":checked") ? SandwichMaker.addTopping(cheesePrice) : SandwichMaker.removeTopping(cheesePrice);
 });
 
 
 //Condiments Checkboxes
 $("input[value='mayo']").click( () => {
 	var condPrice = SandwichMaker.addCond($("input[value='mayo']").val());
-	if( $("input[value='mayo']").is(":checked")) {
-		SandwichMaker.addTopping(condPrice);
-	} else {
-		SandwichMaker.removeTopping(condPrice);
-	}
+	$("input[value='mayo']").is(":checked") ? SandwichMaker.addTopping(condPrice) : SandwichMaker.removeTopping(condPrice);
 });
 $("input[value='mustard']").click( () => {
 	var condPrice = SandwichMaker.addCond($("input[value='mustard']").val());
-	if( $("input[value='mustard']").is(":checked")) {
-		SandwichMaker.addTopping(condPrice);
-	} else {
-		SandwichMaker.removeTopping(condPrice);
-	}
+	$("input[value='mustard']").is(":checked") ? SandwichMaker.addTopping(condPrice) : SandwichMaker.removeTopping(condPrice);
 });
 
 
 //Veggies Checkboxes
 $("input[value='lettuce']").click( () => {
 	var veggiePrice = SandwichMaker.addVeggies($("input[value='lettuce']").val());
-	if( $("input[value='lettuce']").is(":checked")) {
-		SandwichMaker.addTopping(veggiePrice);
-	} else {
-		SandwichMaker.removeTopping(veggiePrice);
-	}
+	$("input[value='lettuce']").is(":checked") ? SandwichMaker.addTopping(veggiePrice) : SandwichMaker.removeTopping(veggiePrice);
 });
 $("input[value='tomato']").click( () => {
 	var veggiePrice = SandwichMaker.addVeggies($("input[value='tomato']").val());
-	if( $("input[value='tomato']").is(":checked")) {
-		SandwichMaker.addTopping(veggiePrice);
-	} else {
-		SandwichMaker.removeTopping(veggiePrice);
-	}
+	$("input[value='tomato']").is(":checked") ? SandwichMaker.addTopping(veggiePrice) :	SandwichMaker.removeTopping(veggiePrice);
 });
 $("input[value='onion']").click( () => {
 	var veggiePrice = SandwichMaker.addVeggies($("input[value='onion']").val());
-	if( $("input[value='onion']").is(":checked")) {
-		SandwichMaker.addTopping(veggiePrice);
-	} else {
-		SandwichMaker.removeTopping(veggiePrice);
-	}
+	$("input[value='onion']").is(":checked") ? SandwichMaker.addTopping(veggiePrice) : SandwichMaker.removeTopping(veggiePrice);
 });
 
 
